eb44abe9e1e2626cc96da7335bf7cce6d1cddc01
高廷旺 <code_butcher@sina.com>
2020-02-18 03:03:59 +0800

    Dubbo Getting Started

--- auto-configure-sample/auto-configure-consumer/src/main/java/com/gtw/dubbo/demo/ConsumerApplication.java
+++ auto-configure-sample/auto-configure-consumer/src/main/java/com/gtw/dubbo/demo/ConsumerApplication.java
@@ -1,12 +1,26 @@
 package com.gtw.dubbo.demo;
 
+import com.gtw.dubbo.rpc.api.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class ConsumerApplication {
+
+    @Reference(version = "1.0.0", url = "dubbo://127.0.0.1:12345")
+    private DemoService demoService;
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> log.info(demoService.sayHello("gtw"));
     }
 }
