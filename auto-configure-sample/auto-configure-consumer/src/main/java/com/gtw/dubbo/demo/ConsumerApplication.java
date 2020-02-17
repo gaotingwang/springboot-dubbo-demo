a32f3c4396e31cabd9220b0cf1371fb11246fd93
高廷旺 <code_butcher@sina.com>
2020-02-18 01:50:57 +0800

    add springboot and dubbo dependencies

--- auto-configure-sample/auto-configure-consumer/src/main/java/com/gtw/dubbo/demo/ConsumerApplication.java
+++ auto-configure-sample/auto-configure-consumer/src/main/java/com/gtw/dubbo/demo/ConsumerApplication.java
@@ -1,8 +1,12 @@
 package com.gtw.dubbo.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConsumerApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ConsumerApplication.class, args);
     }
 }
--- auto-configure-sample/auto-configure-provider/src/main/java/com/gtw/dubbo/demo/ProviderApplication.java
+++ auto-configure-sample/auto-configure-provider/src/main/java/com/gtw/dubbo/demo/ProviderApplication.java
@@ -1,8 +1,12 @@
 package com.gtw.dubbo.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProviderApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ProviderApplication.class, args);
     }
 }
